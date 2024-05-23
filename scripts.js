2872117052c6fb98efe2429451de9538b164de40
ChristopherTsh <156902121+ChristopherTsh@users.noreply.github.com>
2024-05-23 16:46:35 +0200

    creating forEach statement

--- scripts.js
+++ scripts.js
@@ -13,3 +13,12 @@ const products = [
   { product: 'coffee', price: 10 },
   { product: 'tea', price: '' },
 ]
+
+//Log each name
+names.forEach(name => console.log(name));
+ 
+//Log each provence
+provinces .forEach(provinces  => console.log(provinces));
+
+// Log each name with matching province
+names.forEach((name, index) => console.log(`${name} (${provinces[index]})`));
